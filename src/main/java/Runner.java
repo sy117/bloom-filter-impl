078f619201a6759690d65199aa40f326d76bc448
sy117 <07sunilraj@gmail.com>
2023-06-29 23:12:20 +0530

    added plot between number of hash functions and fpp

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,6 @@
+import org.math.plot.Plot2DPanel;
+
+import javax.swing.*;
 import java.util.*;
 
 public class Runner {
@@ -23,7 +26,9 @@ public class Runner {
             with increasing no of hash functions, false positive rate decreases up to a certain point.
             But after that point the rate becomes constant
         */
-        for(int i=1; i<100; i++){
+        double[] x = new double[100];
+        double[] y = new double[100];
+        for(int i=1; i<101; i++){
             BloomFilter bloomFilter = new BloomFilter(size, i);
             addToFilter.forEach((key, value) -> {
                 bloomFilter.add(key);
@@ -35,8 +40,17 @@ public class Runner {
                 }
             }
             double falsePositiveRate = (falsePositive * 1.0) / dataSet.size();
+            x[i-1] = i * 1.0;
+            y[i-1] = falsePositiveRate;
             System.out.println("No of hash functions: " + i + ", false positive rate: " + falsePositiveRate);
         }
 
+        Plot2DPanel plot = new Plot2DPanel();
+        plot.addLinePlot("Num of hash functions vs fpp plot", x, y);
+
+        // put the PlotPanel in a JFrame, as a JPanel
+        JFrame frame = new JFrame("Num of hash functions vs fpp plot");
+        frame.setContentPane(plot);
+        frame.setVisible(true);
     }
 }
